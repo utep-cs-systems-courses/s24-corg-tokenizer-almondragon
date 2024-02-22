d3cd2a2d1a7873bb10029ca56bf0b1347b881894
almondragon <almondragon@miners.utep.edu>
2024-02-22 13:08:16 -0700

    uimain.c updated

--- src/uimain.c
+++ src/uimain.c
@@ -3,9 +3,23 @@
 #include "history.h"
 
 int main(){
-  char words[] = "hello my name is aitiana and this is a test";
-  short length = 100;
-  char **tokenized = tokenize(words);
+  char user_input[101];
+  while(1){
+  printf("> ");
+  fgets(user_input,sizeof(user_input),stdin);
+  int index = 0;
+  while(user_input[index] != '\0'){
+    if(user_input[index] == '\n'){
+      user_input[index] = '\0';
+      break;
+    }
+    index++;
+  }
+  if(user_input[0] == 'e' && user_input[1] == '\0'){
+    printf("Program ended\n");
+    break;
+  }
+  char **tokenized = tokenize(user_input);
   print_tokens(tokenized);
-
+  }
 }
